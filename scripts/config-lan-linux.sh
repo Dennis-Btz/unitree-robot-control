9b7ec1669e92445c8840f33ade5ff618ce6386cf
Freya Aino <freya.aino@pm.me>
2025-11-20 15:32:36 +0100

    initial commit

---
+++ scripts/config-lan-linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo "❌ No argument supplied."
+else
+    echo "✅ trying to add 192.168.123.201/24 to device: $1"
+    sudo ip a add 192.168.123.201/24 dev $1
+fi
